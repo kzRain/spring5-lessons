bbb5f2a10502330f1a5e623e69d3ad45c2513506
alimbetov <51784151+alimbetov@users.noreply.github.com>
2020-07-19 12:47:52 +0600

    Ruslan Alimbetov v7

--- lesson7/spring-validator/src/main/java/kz/cb/spring/lesson7/SingerValidator.java
+++ lesson7/spring-validator/src/main/java/kz/cb/spring/lesson7/SingerValidator.java
@@ -1,6 +1,8 @@
 package kz.cb.spring.lesson7;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
@@ -14,10 +16,19 @@ public class SingerValidator implements Validator {
 
     @Override
     public void validate(Object obj, Errors e) {
+
         ValidationUtils.rejectIfEmpty(e, "firstName", "firstName.empty");
         ValidationUtils.rejectIfEmpty(e, "lastName", "lastName.empty");
         ValidationUtils.rejectIfEmptyOrWhitespace(e,"firstName","firstName.rejectIfEmptyOrWhitespace");
 
+        Singer singer=(Singer)obj;
+        if (singer!=null){
+            System.out.println(" **" + singer.getLastName());
+            if (singer.getLastName().length() > 1 || singer.getLastName().length() <20){
+                e.rejectValue("lastName", "lastName.length 1-20");
+                System.out.println(" lenth error");
+            }
+        }
 
 
     }
