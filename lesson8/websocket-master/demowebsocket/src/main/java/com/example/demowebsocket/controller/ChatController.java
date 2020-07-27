28a66b00a3657fb2acb24122768597b1a65523e4
rus <ruslan.beyganov@gmail.com>
2020-07-27 08:25:29 +0600

    add Websocket Example

---
+++ lesson8/websocket-master/demowebsocket/src/main/java/com/example/demowebsocket/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.example.demowebsocket.controller;
+
+import com.example.demowebsocket.model.Message;
+import com.example.demowebsocket.model.OutputMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public OutputMessage send(Message message) {
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        return new OutputMessage(message.getFrom(), message.getText(), time);
+    }
+}
