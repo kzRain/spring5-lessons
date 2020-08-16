e647c06d13628cfa682dfe8c72a3af0ac5f47b6e
Ruslan.Alimbetov <Ruslan.Alimbetov@kcell.kz>
2020-08-16 10:13:10 +0600

    model 03

--- lesson11/democamundadb/src/main/java/com/camunda/democamundadb/controller/CustumerController.java
+++ lesson11/democamundadb/src/main/java/com/camunda/democamundadb/controller/CustumerController.java
@@ -3,6 +3,7 @@ package com.camunda.democamundadb.controller;
 import com.camunda.democamundadb.model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.logging.Logger;
 
@@ -16,11 +17,25 @@ public class CustumerController {
     @ResponseBody
     public Customer customerparam(@RequestParam String name, @RequestParam Integer age) {
 
+        final String uri = "http://localhost:8080/rest/process-definition/key/Cinema_Proc_ID/start";
+
+        RestTemplate restTemplate = new RestTemplate();
+
         logger.info("name="+name +" ; age="+age);
         
         Customer customer = new Customer(name,age);
 
+        Customer customeresult = restTemplate.getForObject(uri, Customer.class);
+
+
+
         return customer;
     }
+
+
+
+
+
+
 
 }
